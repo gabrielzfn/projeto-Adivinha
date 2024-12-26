034fdabd3a71bd20b86c86a24e7b70e3fe7d29c9
Gabriel Zefino <gabrielzt12@hotmail.com>
2024-12-26 12:24:22 -0300

    feat: Criação do menu de apresentação inicial

---
+++ adivinha.py
@@ -0,0 +1,11 @@
+
+
+print("=" * 50)
+print('Seja bem vindo ao desafio: "Adivinhe o número"!')
+print('Acerte o número entre "1" e "100" para vencer.')
+print("=" * 50)
+
+print("\nPara começar, selecione a dificuldade do desafio.")
+print("Observe que: A dificuldade selecionado irá ditar o número de chances.")
+escolha = input("\n(F) FÁCIL  -  (M) MÉDIO  -  (D) DIFÍCIL  ==== Escolha: ").lower()
+
