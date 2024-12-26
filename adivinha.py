325eeffb1e5b0efc1b2a516ba2a6cfc094df8676
Gabriel Zefino <gabrielzt12@hotmail.com>
2024-12-26 13:12:56 -0300

    feat: Condicional de validação, vitória e derrota

--- adivinha.py
+++ adivinha.py
@@ -29,3 +29,36 @@ print(f'Você terá {chances} chances para adivinhar o número. Boa sorte!')
 numero = random.randint(1, 100)
 # Variável que armazena o número de rodadas (chances utilizadas)
 count  = 1
+
+
+# Condicional para vencer, perder e validação
+while count <= chances:
+    print('')
+    print("-" * 50)
+    print(f'Rodada: {count}')
+    print("-" * 50)
+    chute = input('Digite seu chute: ')
+    
+    try:
+        chute_int   = int(chute)
+    except:
+        print('Digite um número válido')
+        continue
+    
+    
+    if chute_int    == numero:
+        print('Parabéns!! Você acertou.')
+        break
+    
+    
+    if chute_int  <= numero:
+        print('Chute abaixo do número correto')
+    else:
+        print('Chute acima do número correto')
+    
+    
+    if count == chances:
+        print('Você perdeu!')
+        break
+    
+    count +=1
